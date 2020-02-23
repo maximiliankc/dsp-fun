ff52a9ae5d3b81b1296d3ddc05f45c16578a21f3
maximiliankc <cornmaxi@gmail.com>
2020-02-23 21:17:36 +1100

    added in an impulse funciton, a function to plot FT

--- dsp/dsp.py
+++ dsp/dsp.py
@@ -8,7 +8,7 @@ def sinc_kernel(cutoff, halflength): # cutoff should be between 0 and 1, TODO wi
     # return the normalised kernel
     return out/np.sum(out)
 
-def filt(signal, kernel, centre=0): # not dealing with edge effects at all
+def fir_filt(signal, kernel, centre=0): # not dealing with edge effects at all, chopping the edges off
     out = np.convolve(signal, kernel)
     return out[centre:centre+signal.shape[0]]
 
@@ -22,21 +22,35 @@ def resample(signal, N, M, kernelWidth=0): # resize by N/M
     if N > M: # if the line is bigger than the original, restrict to the original bandwidth 
         kernel = sinc_kernel(1/N, kernelWidth)
     else: # if it's smaller than the original, restrict to the new bandwidth
-        kernel = sinc_kernel(1/M, kernelWidth)
-    bigger = filt(bigger, N*kernel, kernelWidth)
+        kernel = M*sinc_kernel(1/M, kernelWidth)
+    bigger = fir_filt(bigger, N*kernel, kernelWidth)
     # then decimate:
     smaller = np.zeros(bigger.shape[0]//M) # floor division
     for k in range(smaller.shape[0]):
         smaller[k] = bigger[M*k]
     return smaller
 
+def impulse(N):
+    x = np.zeros(N)
+    x[0] = 1
+    return x
+
+def plotFT(x):
+    X = np.fft.fft(x)/(len(x)**0.5)
+    w = np.linspace(0, 2.0, len(x))
+    plt.subplot(211)
+    plt.plot(w, np.abs(X))
+    plt.subplot(212)
+    plt.plot(w, np.angle(X))
+    plt.show()
+
 def main():
-    fs1 = 1000
-    fs2 = 2000
     x = np.arange(100)
-    y = np.sin(2*np.pi*500*x/fs2)
-    y2 = resample(y,2,1, 10)
-    #y2 = sinc_kernel(0.1, 20)
+    y = impulse(100)
+    y2 = resample(y, 1, 2, 10)
+    print(f"length of y: {len(y)}, length of y2: {len(y2)}")
+    plotFT(y)
+    plotFT(y2)
     plt.plot(y)
     plt.plot(y2)
     plt.show()
