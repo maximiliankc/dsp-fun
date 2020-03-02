7831cab4744f26da482303f43098dc249022b7e4
maximiliankc <cornmaxi@gmail.com>
2020-03-02 21:07:53 +1100

    A little bit of formatting, started on an iir filter

--- dsp/dsp.py
+++ dsp/dsp.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 # W = J/sample
 
+## Filtering
+
 def sinc_kernel(cutoff, halflength): # cutoff should be between 0 and 1, TODO window function, low-pass/highpass
     n = np.arange(-halflength, halflength + 1)
     # get the sinc function:
@@ -14,7 +16,22 @@ def fir_filt(signal, kernel, centre=0): # not dealing with edge effects at all, 
     out = np.convolve(signal, kernel)
     return out[centre:centre+signal.shape[0]]
 
+def iir_filt(signal, a, b): # not dealing with edge effects at all, chopping the edges off
+    # y[n] = ((m=0->∞)Σa_m*x[n-m] + (m=1->∞)Σb_m*y[n-m])/b_0
+    # v[n] = (a * x)[n]
+    # y[n] = (v[n] + <y_n,b>)/b_0
+    v = fir_filt(signal, a)
+    b1 = b[1:]
+    print(f"b: {b}, b1: {b1}")
+    out = np.empty(signal.shape)
+    for n in range(signal.size):
+        y = np.zeros()
+        out[n] = np.vdot(b1,[out[n-1:-1:0], np.zeros(b1.size-n)]) + v[n]
+        
+
+
 def resample(signal, N, M, kernelWidth=0): # resize by N/M
+    iir_filt(np.array([1,0,0]), np.array([1, 2, 3]), np.array([1,2,3,4,5,6]))
     # first, interpolate:
     bigger = np.zeros(N*signal.shape[0])
     for k in range(signal.shape[0]):
@@ -28,14 +45,21 @@ def resample(signal, N, M, kernelWidth=0): # resize by N/M
     bigger = fir_filt(bigger, N*kernel, kernelWidth)
     # then decimate:
     smaller = np.zeros(bigger.shape[0]//M) # floor division
-    for k in range(smaller.shape[0]):
+    for k in range(smaller.size):
         smaller[k] = bigger[M*k]
     return smaller
+
+#synthesis
 
 def impulse(N):
     x = np.zeros(N)
     x[0] = 1
     return x
+
+def white_noise(std, length):
+    return np.random.normal(0, std, length)
+
+# Analysis
 
 def energy(x):
     # returns energy, average power
@@ -67,7 +91,8 @@ def plotFT(x):
     return [X, w]
 
 def main():
-    y = impulse(100)
+    #y = impulse(100)
+    y = white_noise(1,100)
     y2 = resample(y, 2, 1, 10)
     print(f"length of y: {len(y)}, length of y2: {len(y2)}")
     [Y,_] = plotFT(y)
